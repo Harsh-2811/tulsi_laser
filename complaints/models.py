0d830d6bd0bdcfe9f21d7f04efe2308d2d54335d
MITESHDABHI <dabhimitesh18@gmail.com>
2023-10-07 10:09:12 +0530

    added ComplainOutcome model

--- complaints/models.py
+++ complaints/models.py
@@ -20,3 +20,23 @@ class Complain(models.Model):
     status = models.IntegerField(choices=Statuses.choices, default=Statuses.new)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class ComplainOutcome(models.Model):
+    class Complaint_types(models.TextChoices):
+        genuine = "Genuine", "Genuine"
+        fake = "Fake", "Fake"
+        
+    complain = models.ForeignKey(Complain, on_delete=models.CASCADE)
+    technician = models.ForeignKey(Technician, on_delete=models.CASCADE)
+    complaint_type = models.CharField(choices=Complaint_types.choices, max_length=50)
+    challan = models.IntegerField(null=True, blank=True) 
+    remark = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    signature = models.CharField(max_length=255)
+    alignment = models.BooleanField(default=False)
+    ampere = models.DecimalField(max_digits=10, decimal_places=2) 
+    temp = models.DecimalField(max_digits=5, decimal_places=2)
+    voltage = models.DecimalField(max_digits=8, decimal_places=2)
+    water_filter = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
