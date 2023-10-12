e70fb5c8d7ccab2a0a765ba77606d24d8418bbc6
MITESHDABHI <dabhimitesh18@gmail.com>
2023-10-12 12:34:54 +0530

    changed the API response

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from complaints.models import Complain, ComplainOutcome
 from django_filters import rest_framework as filters
+from customers.models import Machine, Customer
+from users.models import Technician
 
 
 class ComplainFilter(filters.FilterSet):
@@ -17,7 +19,29 @@ class UpdateStatusSerializer(serializers.ModelSerializer):
         fields = ['status']
 
 
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+
+class MachineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Machine
+        fields = '__all__'
+
+
+class TechnicianSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Technician
+        fields = '__all__'
+
+
 class ComplainSerializer(serializers.ModelSerializer):
+    customer = CustomerSerializer()
+    machine = MachineSerializer()
+    technician = TechnicianSerializer()
+
     class Meta:
         model = Complain
         fields = '__all__'
