c83c1a0dc654ef75a9f8fae74bb9eba5d1100b51
MITESHDABHI <dabhimitesh18@gmail.com>
2023-11-22 09:29:12 +0530

    changed the database

--- tulsi_laser_tech/settings.py
+++ tulsi_laser_tech/settings.py
@@ -87,32 +87,22 @@ WSGI_APPLICATION = 'tulsi_laser_tech.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tulsi_laser',
+        'USER': 'svlaser',
+        'PASSWORD': 'Svlaser@502',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'tulsi_laser',
-#         'USER': 'svlaser',
-#         'PASSWORD': 'Svlaser@502',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'tulsi_laser',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
