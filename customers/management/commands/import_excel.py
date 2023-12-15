10f48034667218f978ffb0db9b8d87c2d9c3504a
Harsh Patel <harshpatel@Harshs-MacBook-Pro.local>
2023-12-15 14:14:12 +0530

    Update new sheet data

--- customers/management/commands/import_excel.py
+++ customers/management/commands/import_excel.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from customers.models import Customer, Machine, MachineType
 import os
 import pandas as pd
+import numpy as np
 from users.models import User
 
 class Command(BaseCommand):
@@ -14,19 +15,22 @@ class Command(BaseCommand):
         parser.add_argument('-s', '--skip_row', type=str )
 
     def handle(self, *args, **kwargs):
-        file_path = os.path.join(settings.BASE_DIR, "machine_details.xlsx")
+        file_path = os.path.join(settings.BASE_DIR, "all_machine_list.xlsx")
         sheet_name = kwargs['sheet_name']
         skip_row = kwargs['skip_row']
         print(sheet_name)
         xls = pd.ExcelFile(file_path)
-        df1 = pd.read_excel(xls, sheet_name,skiprows=int(skip_row))
+        df1 = pd.read_excel(xls, sheet_name,skiprows=int(skip_row), keep_default_na=False)
+        df1.fillna(0, inplace=True)
       
         print(f"Columns in Excel: {df1.columns}")
-
+        MachineType.objects.all().delete()
+        Machine.objects.all().delete()
+        Customer.objects.all().delete()
         for index, row in df1.iterrows():
             # Use correct column names
             __type = row['MACHINE TYPE'] if row.get('MACHINE TYPE') else row['MACHINE NAME']
-            if not __type and __type == "nan":
+            if not __type or __type == "nan":
                 continue
             _type = str(__type).strip()
             try:
@@ -43,9 +47,13 @@ class Command(BaseCommand):
                 except User.DoesNotExist:
                     user = User.objects.create_user(username=row['PARTY NAME'], email=email, first_name=row['PARTY NAME'], password="TulsiLaser@123", role=User.Roles.customer)
 
-                customer = Customer.objects.create(company_name=row['PARTY NAME'], address=row.get('ADD.'), user=user)
+                if not row['CONTACT NO']:
+                
+                    customer = Customer.objects.create(company_name=row['PARTY NAME'], address=row.get('ADDRESS'), user=user)
+                else:
+                    customer = Customer.objects.create(company_name=row['PARTY NAME'], address=row.get('ADDRESS'), user=user, company_mobile_no=f"+91 {row['CONTACT NO']}" if row['CONTACT NO'] not in ("nan", None) else "")
 
             try:
-                machine = Machine.objects.get(code=row['TYPE NO'] if row.get('TYPE NO') else row['MACHINE NO'])
+                machine = Machine.objects.get(code=row['TYPE NO'] if row.get('TYPE NO') else row['MACHINE NAME'])
             except Machine.DoesNotExist:
-                machine = Machine.objects.create(code=row['TYPE NO'] if row.get('TYPE NO') else row['MACHINE NO'], machine_type=machine_type, purchase_date=row['DATE'], customer=customer, duration=10, warranty = Machine.Warranty.yearly)
+                machine = Machine.objects.create(code=row['TYPE NO'] if row.get('TYPE NO') else row['MACHINE NAME'], machine_type=machine_type, purchase_date=row['DATE'], customer=customer, duration=10, warranty = Machine.Warranty.yearly)
