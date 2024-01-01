cda731e7fbadab9b69c16589bc43aebb606a7eb4
Harsh Patel <harshpatel@Harshs-MacBook-Pro.local>
2024-01-01 19:17:46 +0530

    Change DB Creds

--- tulsi_laser_tech/settings.py
+++ tulsi_laser_tech/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zlg(cj(n@i5&szr@xfi$nwl4e475kf-1l@g!@$k#&4(-2&)q#a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['http://64.227.144.27']
+CSRF_TRUSTED_ORIGINS = ['http://64.227.144.27', 'https://app.tulsilaser.com']
 
 # Application definition
 
@@ -155,10 +155,10 @@ SPECTACULAR_SETTINGS = {
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT=os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
