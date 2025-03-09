27a3c7dbdada92710b92a6ae04ba1976edb6f920
Harsh Patel <harshpatel@Harshs-MacBook-Pro.local>
2025-03-09 17:28:19 +0530

    Set Debug True

--- tulsi_laser_tech/settings.py
+++ tulsi_laser_tech/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zlg(cj(n@i5&szr@xfi$nwl4e475kf-1l@g!@$k#&4(-2&)q#a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['http://139.59.42.151', 'https://app.tulsilaser.com']
@@ -87,22 +87,22 @@ WSGI_APPLICATION = 'tulsi_laser_tech.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tulsi_laser',
-        'USER': 'tulsi_laser',
-        'PASSWORD': 'TulsiLaser@123',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'tulsi_laser',
+#         'USER': 'tulsi_laser',
+#         'PASSWORD': 'TulsiLaser@123',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -155,10 +155,10 @@ SPECTACULAR_SETTINGS = {
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS=[
-#     os.path.join(BASE_DIR, 'static')
-# ]
+# STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
